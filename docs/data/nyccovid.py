79411e5742e10bbb655c757ca3d431ee788f42d9
Ege Cavusoglu <egecavusoglu99@gmail.com>
2020-11-30 11:06:43 +0300

    id field for metro.csv added.

--- docs/data/nyccovid.py
+++ docs/data/nyccovid.py
@@ -5,19 +5,19 @@ import csv
 
 # Clean up of metro data to retrieve necessary fields and sort according to date.
 
-newcsv = [] # Rearranging initial csv in an array.
+newcsv = []  # Rearranging initial csv in an array.
 
 # Read the unprocessed data.
 with open('metro.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         if row["date"] >= "2020-08-01":
-            row = {'date': row['date'], 'stop_name': row['stop_name'], 'gtfs_longitude': row['gtfs_longitude'], 'gtfs_latitude': row['gtfs_latitude'], 'entries': row['entries'], 'exits': row['exits']}
+            row = {'id': f'{row["gtfs_longitude"]}{row["gtfs_latitude"]}'  ,'date': row['date'], 'stop_name': row['stop_name'], 'gtfs_longitude': row['gtfs_longitude'], 'gtfs_latitude': row['gtfs_latitude'], 'entries': row['entries'], 'exits': row['exits']}
             newcsv.append(row)
 
 newcsv.sort(key=lambda x: x["date"]) # Sort the data by date for easier processing.
 
-fieldnames = ['date', 'stop_name', 'gtfs_longitude', 'gtfs_latitude', 'entries', 'exits']  # declare the field names for the output csv.
+fieldnames = ['id', 'date', 'stop_name', 'gtfs_longitude', 'gtfs_latitude', 'entries', 'exits']  # declare the field names for the output csv.
 
 # Output to a new file.
 output_file = open('output/metro.csv','w')
